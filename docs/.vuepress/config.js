061945737cae5470bbf65ef77557161833b2d3b4
Dayo <23241655+DayoWong0@users.noreply.github.com>
2020-10-26 09:21:05 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
     ], //在移动端，搜索框在获得焦点时会放大
   ],
-  base: '/blog/',
+  // base: '/blog/', 
   theme: "reco", //选择主题‘reco’
   themeConfig: {
     type: "blog", //选择类型博客
